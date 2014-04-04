f7ea5c5f8ef41c1c2a200f31cbf4b36ccfb73920
Luke McDonald <thelukemcdonald@gmail.com>
2014-04-03 19:35:26 -0500

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== Plugin Name ===
+Contributors: thelukemcdonald
+Tags: WPCOM, testing
+Requires at least: 2.9
+Stable tag: 1.0.0
+Tested up to: 3.9
+
+Test your themes in WPCOM mode.
+
+== Description ==
+
+If available, this plugin will load a `wpcom.php` compatibility file from the `inc/` or `includes/` folder. The plugin also defines the `IS_WPCOM` constant.
+
+For more control, use [Plugin Toggle](http://wordpress.org/plugins/plugin-toggle/) to toggle the plugin on and off.
+
+== Installation ==
+
+Upload, Activate
+
+== Changelog ==
+
+= 0.1 =
+Initial version.
+
+
---
+++ wpcom-tester.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: WPCOM Tester
+ * Plugin URI: http://wordpress.org/extend/plugins/wpcom-tester/
+ * Description: This plugin loads <code>wpcom.php</code> compatibility file from <code>inc/</code> or <code>includes/</code> folder if availble. The plugin also defines the <code>IS_WPCOM</code> constant.
+ * Author: <a href="http://lukemcdonald.com">Luke McDonald</a>
+ * Version: 1.0.0
+ *
+ * @package WPCOM_Tester
+ * @author Luke McDonald
+ * @version 1.0.0
+ */
+
+if ( ! defined( 'IS_WPCOM' ) ) {
+	define( 'IS_WPCOM', true );
+}
+
+function wpcom_tester_setup() {
+	locate_template( array( 'inc/wpcom.php', 'includes/wpcom.php' ), true );
+}
+add_action( 'after_setup_theme', 'wpcom_tester_setup' );
+
